6f626c92cdc312cfad770a171b4b46726800ddbc
vvprivalov <vv.privalov@yandex.ru>
2022-04-14 13:09:41 +0300

    2

--- src/main/java/UseGit.java
+++ src/main/java/UseGit.java
@@ -1,5 +1,6 @@
 public class UseGit {
     public static void main(String[] args) {
         System.out.println("\n\nВывожу первую строку и делаю коммит");
+        System.out.println("\n\nВывожу вторую строку и делаю коммит");
     }
 }
